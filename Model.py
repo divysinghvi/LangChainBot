3a972e59db976511d8bac73ab0e15f9558ffa6d4
Divy Singhvi <divysinghvi5@gmail.com>
2025-05-04 23:27:10 +0530

    Created Langchain model

---
+++ Model.py
@@ -0,0 +1,15 @@
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+
+class LangchainGPTBotModel:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.llm = OpenAI(temperature=0.7, openai_api_key=self.api_key)
+
+    def create_chain(self, prompt_template: str):
+        prompt = PromptTemplate(input_variables=["input"], template=prompt_template)
+        return LLMChain(llm=self.llm, prompt=prompt)
+
+    def generate_response(self, chain: LLMChain, user_input: str):
+        return chain.run(input=user_input)
